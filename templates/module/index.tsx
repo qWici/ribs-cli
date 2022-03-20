898615fa5fc5649cfe64642e322d3d7e4ca3c3f9
Ivan Kucher <ivan.kucher@gen.tech>
2022-03-20 14:35:57 +0200

    fix: import only React instead of full lib

--- templates/module/index.tsx
+++ templates/module/index.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 
 import { usePascalCaseModuleNameInteractor } from "./interactor";
 import { PascalCaseModuleNameRouter } from "./router";
